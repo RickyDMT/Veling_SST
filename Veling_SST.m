426194443cb1272bb438d47f254edc6aaf4f9de2
Erik Knight <elk@uoregon.edu>
2014-10-08 08:54:28 -0700

    Moved from Mac
    Implemented changes to conditions; added subj info collection;

--- Veling_SST.m
+++ Veling_SST.m
@@ -2,31 +2,32 @@ function Veling_SST(varargin)
 
 global KEY COLORS w wRect XCENTER YCENTER PICS STIM SST trial
 
-prompt={'SUBJECT ID'};
-defAns={'4444'};
+prompt={'SUBJECT ID' 'Condition (1 or 2)' 'Session (1, 2, or 3)'};
+defAns={'4444' '' ''};
 
 answer=inputdlg(prompt,'Please input subject info',1,defAns);
 
 ID=str2double(answer{1});
+COND = str2double(answer{2});
+SESS = str2double(answer{3});
 
-%Get condition assignment
+%Make sure input data makes sense.
+% try
+%     if SESS > 1;
+%         %Find subject data & make sure same condition.
+%         
+%     end
+% catch
+%     error('Subject ID & Condition code do not match.');
+% end
+
 
 rng(ID); %Seed random number generator with subject ID
+d = clock;
 
 KEY = struct;
 KEY.rt = KbName('SPACE'); %To end random trial selection
-% KEY.one = KbName('1!'); 
-% KEY.two = KbName('2@'); 
-% KEY.tres = KbName('3#');
-% KEY.four = KbName('4$'); 
-% KEY.five = KbName('5%');
-% KEY.six = KbName('6^');
-% KEY.sev = KbName('7&'); 
-% KEY.eight = KbName('8*'); 
-% KEY.nine = KbName('9('); 
-% KEY.zero = KbName('0)'); 
-% KEY.yes = KbName('y'); 
-% KEY.no = KbName('n');
+
 
 COLORS = struct;
 COLORS.BLACK = [0 0 0];
@@ -45,27 +46,32 @@ STIM.trials = 32;
 STIM.trialdur = 1.250;
 
 % Load in pics
-%This will be based off of condition: Control = bird, flowers, etc.
-%Modulate based off of condition assignment
-%Change PICS.in.XXXX to more bland names (Go, NoGo, Neut)
+
 PICS =struct;
-PICS.in.good = dir('*good.jpg');
-PICS.in.bad = dir('*bad.jpg');
-PICS.in.h2o = dir('*water.jpg');
+if COND == 1;                   %Condtion = 1 is food. 
+    PICS.in.go = dir('*good.jpg');
+    PICS.in.no = dir('*bad.jpg');
+    PICS.in.neut = dir('*water.jpg');
+elseif COND == 2;               %Condition = 2 is not food (birds/flowers)
+    PICS.in.go = dir('*bird.jpg');
+    PICS.in.no = dir('*flowers.jpg');
+    PICS.in.neut = dir('*mam.jpg');
+end
 picsfields = fieldnames(PICS.in);
 
 SST = struct;
 
-trial_types = [ones(14,1); repmat(2,14,1); repmat(3,4,1)]; %1 = good food; 2 = bad food; 3 = water
-gonogo = [ones(14,1); zeros(14,1)];
+trial_types = [ones(14,1); repmat(2,14,1); repmat(3,4,1)];  %1 = go; 2 = no; 3 = neutral/variable
+gonogo = [ones(14,1); zeros(14,1)];                         %1 = go; 0 = nogo;
 gonogoh20 = BalanceTrials(sum(trial_types==3),1,[0 1]);
 gonogo = [gonogo; gonogoh20];
-%When appropriate pics are found, update these to randperm(length(Pics.in.good),14) where '14' is # of desired pics of each type per block
-%NEEDS QUESTION ANSWERED: All pics once, or all pics randomly (sample w/replacement)
-%ELK 9/30/14: All pics will be displayed once!
-piclist = [randperm(length(PICS.in.good)) randperm(length(PICS.in.good))];
-piclist = [piclist randperm(length(PICS.in.bad)) randperm(length(PICS.in.bad))];
-piclist = [piclist randperm(length(PICS.in.h2o))]';
+%When appropriate pics are found, update these to
+%randperm(length(Pics.in.go)) to make a long list of all the pic numbers
+%randomized.
+%Elk: Using all pics once per session (randomized across blocks)
+piclist = [randperm(length(PICS.in.go)) randperm(length(PICS.in.go))];
+piclist = [piclist randperm(length(PICS.in.no)) randperm(length(PICS.in.no))];
+piclist = [piclist randperm(length(PICS.in.neut))]';
 trial_types = [trial_types gonogo piclist];
 
 for g = 1:STIM.blocks;
@@ -74,8 +80,15 @@ for g = 1:STIM.blocks;
     SST.var.picnum(1:STIM.trials,g) = shuffled(:,3);
     SST.var.GoNoGo(1:STIM.trials,g) = shuffled(:,2);
 end
-    SST.data.rt = [];
-    SST.data.correct = [];
+
+    SST.data.rt = zeros(STIM.trials, STIM.blocks);
+    SST.data.correct = zeros(STIM.trials, STIM.blocks)-999;
+    SST.data.avg_rt = zeros(STIM.blocks,1);
+    SST.data.info.ID = ID;
+    SST.data.info.cond = COND;               %Condtion 1 = Food; Condition 2 = animals
+    SST.data.info.session = SESS;
+    SST.data.info.date = sprintf('%s %2.0f:%02.0f',date,d(4),d(5));
+    
 
 
 commandwindow;
@@ -127,8 +140,8 @@ KbName('UnifyKeyNames');
 %% Set frame size;
 STIM.framerect = [XCENTER-330; YCENTER-330; XCENTER+330; YCENTER+330];
 
-% %moved to block by block input; will be based on pre-determined, random
-% %selection of images.
+%moved to block by block input; will be based on pre-determined, random
+%selection of images.
 % for n = 1:length(picsfields);
 %     curr_field = picsfields{n};
 %     PICS.out.(curr_field).raw = [];
@@ -148,10 +161,10 @@ DrawFormattedText(w,'The stop signal task is about to begin.\nPress any key to c
 Screen('Flip',w);
 KbWait();
 Screen('Flip',w);
-WaitSecs(.5);
+WaitSecs(1);
 
 %% Instructions
-DrawFormattedText(w,'You see pictures with either a blue or gray border around them.\nPlease the press the space bar as quickly & accurately as you can\nBUT only if you see a BLUE bar around the image.\nDo not press if you see a gray bar.\nPress any key to continue.','center','center',COLORS.WHITE,300);
+DrawFormattedText(w,'You will see pictures with either a blue or gray border around them.\nPlease the press the space bar as quickly & accurately as you can\nBUT only if you see a BLUE bar around the image.\nDo not press if you see a gray bar.\nPress any key to continue.','center','center',COLORS.WHITE,300);
 Screen('Flip',w);
 KbWait();
 
@@ -171,7 +184,59 @@ for block = 1:STIM.blocks;
         Screen('Flip',w);
         WaitSecs(.5);
     end
-    %Inter-block info here, re: RT, accuracy, etc.
+    %Inter-block info here, re: Display RT, accuracy, etc.
+    %Calculate block RT
+    c = SST.data.correct(:,block) == 1;
+    corr_count = sprintf('Block %d Number Correct:\t%d',block,length(find(c)));
+    block_go = SST.var.GoNoGo(:,block) == 1;
+    if isempty(c(c==1))
+        %Don't try to calculate avg RT, they got them all wrong (WTF?)
+        %Display "N/A" for this block's RT.
+        ibt_rt = sprintf('Block %d Average RT:\tN/A',block);
+    else
+        blockrts = SST.data.rt(:,block);
+        blockrts = blockrts(c & block_go);
+        avg_rt_block = mean(blockrts)*100;
+        ibt_rt = sprintf('Block %d, Average RT:\t%0.3f',block,avg_rt_block);
+    end
+    
+    DrawFormattedText(w,corr_count,10,10,COLORS.WHITE);
+    DrawFormattedText(w,ibt_rt,10,10,COLORS.WHITE);
+    
+    if block > 1
+        % Also display rest of block data summary
+    end
+    
+    Screen('Flip',w);
+        
+    
+end
+
+%% Save all the data
+
+%Export pro.DMT to text and save with subject number.
+%find the mfilesdir by figuring out where show_faces.m is kept
+[mfilesdir,~,~] = fileparts(which('Veling_SST.m'));
+
+%get the parent directory, which is one level up from mfilesdir
+[parentdir,~,~] =fileparts(mfilesdir);
+
+
+%create the paths to the other directories, starting from the parent
+%directory
+% savedir = [parentdir filesep 'Results\proDMT\'];
+savedir = [parentdir filesep 'Results' filesep];
+
+save([savedir 'SST_' num2str(ID) '_' num2str(SESS) '.mat'],'SST');
+
+DrawFormattedText(w,'Thank you for participating\n in this part of the study!','center','center',COLORS.WHITE);
+Screen('Flip', w);
+
+if w~=w2
+    
+    DrawFormattedText(w2,'Thank you for participating\n in this part of the study!','center','center',COLORS.WHITE);
+    Screen('Flip', w2);
+    
 end
 
 sca
@@ -209,18 +274,18 @@ global w STIM PICS COLORS SST KEY
             trial_rt = GetSecs() - RT_start;
             
             Screen('DrawTexture',w,PICS.out(trial).texture);
-            old = Screen('TextSize',w,60);
+            old = Screen('TextSize',w,40);
             if SST.var.GoNoGo(trial,block) == 0;
                 Screen('FrameRect',w,COLORS.NO,STIM.framerect,20);
                 DrawFormattedText(w,'X','center','center',COLORS.RED);
-                Screen('TextSize',w,old);
                 correct = 0;
             else
-%                 Screen('FrameRect',w,COLORS.GO,STIM.framerect,20);
+                Screen('FrameRect',w,COLORS.GO,STIM.framerect,20);
 %                 DrawFormattedText(w,'+','center','center',COLORS.GREEN);
                 correct = 1;
             end
             Screen('Flip',w');
+            Screen('TextSize',w,old);
             WaitSecs(.5);
             break;
         end
@@ -236,6 +301,7 @@ global w STIM PICS COLORS SST KEY
             correct = 1;
         else
             Screen('FrameRect',w,COLORS.GO,STIM.framerect,20);
+%             DrawFormattedText(w,'Please Click Faster','center','center',COLORS.RED);
             DrawFormattedText(w,'X','center','center',COLORS.RED);
             Screen('Flip',w);
             correct = 0;
@@ -257,12 +323,13 @@ global PICS SST w
         pic = SST.var.picnum(j,block);
         switch SST.var.trial_type(j,block)
             case {1}
-                PICS.out(j).raw = imread(getfield(PICS,'in','good',{pic},'name'));
-                PICS.out(j).texture = Screen('MakeTexture',w,PICS.out(j).raw);
+                PICS.out(j).raw = imread(getfield(PICS,'in','go',{pic},'name'));
+%                 %I think this is is covered outside of switch/case
+%                 PICS.out(j).texture = Screen('MakeTexture',w,PICS.out(j).raw);
             case {2}
-                PICS.out(j).raw = imread(getfield(PICS,'in','bad',{pic},'name'));
+                PICS.out(j).raw = imread(getfield(PICS,'in','no',{pic},'name'));
             case {3}
-                PICS.out(j).raw = imread(getfield(PICS,'in','h2o',{pic},'name'));
+                PICS.out(j).raw = imread(getfield(PICS,'in','neut',{pic},'name'));
         end
         PICS.out(j).texture = Screen('MakeTexture',w,PICS.out(j).raw);
     end
